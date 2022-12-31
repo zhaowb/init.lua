ce679455b235f2f484408d61c02762538066ed97
zhaowb <zhaowb@gmail.com>
2023-01-01 12:41:34 +1300

    remove rust from treesitter

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -25,7 +25,7 @@ return {
         config = function()
             require'nvim-treesitter.configs'.setup {
                 -- A list of parser names, or "all"
-                ensure_installed = { "help", "javascript", "typescript", "python", "c", "lua", "rust" },
+                ensure_installed = { "help", "javascript", "typescript", "python", "c", "lua" },
 
                 -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
