be5d17b56e08360280f185e95f81ce7a18603ca3
Wenbo Zhao <zhaowb@gmail.com>
2023-01-04 17:00:06 +1300

    remove unused comment

--- lua/config/plugins/null-ls.lua
+++ lua/config/plugins/null-ls.lua
@@ -16,9 +16,3 @@ return {
         })
     end
 }
-
--- function M.has_formatter(ft)
---     local sources = require("null-ls.sources")
---     local available = sources.get_available(ft, "NULL_LS_FORMATTING")
---     return #available > 0
--- end
