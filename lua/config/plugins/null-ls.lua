68ccfaa3f7d3fa76f543e38949a46c3b06865abf
Wenbo Zhao <zhaowb@gmail.com>
2023-01-04 16:55:41 +1300

    move lsp, treesitter to file

--- lua/config/plugins/null-ls.lua
+++ lua/config/plugins/null-ls.lua
@@ -1,48 +1,24 @@
 -- https://github.com/folke/dot/blob/master/config/nvim/lua/config/plugins/null-ls.lua
-local M = {
+return {
     "jose-elias-alvarez/null-ls.nvim",
     dependencies = {
         'nvim-lua/plenary.nvim'
     },
+    config = function()
+        local nls = require("null-ls")
+        nls.setup({
+            sources = {
+                -- [isort]
+                -- rely on external 'isort' command
+                -- use `pipx install isort` to make isort globally available
+                nls.builtins.formatting.isort,
+            },
+        })
+    end
 }
 
-function M.setup(options)
-    local nls = require("null-ls")
-    nls.setup({
-        debounce = 150,
-        save_after_format = false,
-        sources = {
-            -- nls.builtins.formatting.prettierd,
-            nls.builtins.formatting.stylua,
-            nls.builtins.formatting.fish_indent,
-            -- nls.builtins.formatting.fixjson.with({ filetypes = { "jsonc" } }),
-            -- nls.builtins.formatting.eslint_d,
-            -- nls.builtins.diagnostics.shellcheck,
-            nls.builtins.formatting.shfmt,
-            nls.builtins.diagnostics.markdownlint,
-            -- nls.builtins.diagnostics.luacheck,
-            nls.builtins.formatting.prettierd.with({
-                filetypes = { "markdown" }, -- only runs `deno fmt` for markdown
-            }),
-            nls.builtins.diagnostics.selene.with({
-                condition = function(utils)
-                    return utils.root_has_file({ "selene.toml" })
-                end,
-            }),
-            -- nls.builtins.code_actions.gitsigns,
-            nls.builtins.formatting.isort,
-            nls.builtins.formatting.black,
-            nls.builtins.diagnostics.flake8,
-        },
-        on_attach = options.on_attach,
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", ".git"),
-    })
-end
-
-function M.has_formatter(ft)
-    local sources = require("null-ls.sources")
-    local available = sources.get_available(ft, "NULL_LS_FORMATTING")
-    return #available > 0
-end
-
-return M
+-- function M.has_formatter(ft)
+--     local sources = require("null-ls.sources")
+--     local available = sources.get_available(ft, "NULL_LS_FORMATTING")
+--     return #available > 0
+-- end
