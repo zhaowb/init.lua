08f8e868147db4bd787cd2b4aff9a9189ff33e97
Wenbo Zhao <zhaowb@gmail.com>
2023-01-06 11:46:14 +1300

    use pyright + black

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
         { 'williamboman/mason-lspconfig.nvim',
             config = function()
                 require("mason-lspconfig").setup {
-                    ensure_installed = { "pylsp" }
+                    ensure_installed = { "pyright", "sumneko_lua" }
                 }
             end
         },
--- lua/config/plugins/null-ls.lua
+++ lua/config/plugins/null-ls.lua
@@ -5,15 +5,21 @@ return {
         'nvim-lua/plenary.nvim',
         { 'williamboman/mason.nvim',
             config = function()
-                mason = require("mason")
+                local mason = require("mason")
                 mason.setup()
-                make_sure_install = {
-                    "isort",
+                -- following logic are copied from mason-lspconfig/ensure_installed.lua
+                local make_sure_install = {
+                    "isort", "black",
                 }
-                mason_reg = require("mason-registry")
-                for _, pkg in ipairs(make_sure_install) do
-                    if not mason_reg.is_installed(pkg) then
-                        vim.cmd("MasonInstall " .. pkg)
+                local mason_reg = require("mason-registry")
+                local notify = require "mason-core.notify"
+                for _, pkg_name in ipairs(make_sure_install) do
+                    local ok, pkg = pcall(mason_reg.get_package, pkg_name)
+                    if ok then
+                        if not pkg:is_installed() then
+                            notify(("[mason.nvim] installing %s"):format(pkg_name))
+                            pkg:install {}
+                        end
                     end
                 end
             end
@@ -21,6 +27,7 @@ return {
     },
     config = function()
         local nls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
         nls.setup({
             sources = {
                 -- [isort]
@@ -28,7 +35,22 @@ return {
                 -- either use mason install isort as above, or
                 -- use `pipx install isort` to make isort globally available
                 nls.builtins.formatting.isort,
+                nls.builtins.formatting.black,
             },
+            -- format before save, copied from
+            -- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end,
+                    })
+                end
+            end,
         })
     end
 }
